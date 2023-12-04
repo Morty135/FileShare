caf3ff8c3d1eb1bd23745f12304e2d7301129ffa
Morty135 <morty135135135@gmail.com>
2023-12-04 11:30:43 +0100

    AddedAndEditedCodeFromWebDev

--- src/server.js
+++ src/server.js
@@ -1,17 +1,51 @@
-const express = require("express");
-const path = require('path');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-const app = express();
+require("dotenv").config()
+const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
 
-const port = 3000;
-const staticPath = path.join(__dirname, 'public');
+const express = require("express")
+const app = express()
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(staticPath, 'html/index.html'));
-});
+const upload = multer({ dest: "uploads" })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+mongoose.connect(process.env.DATABASE_URL)
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+
+  const file = await File.create(fileData)
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+})
+
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password")
+      return
+    }
+  }
+
+  file.downloadCount++
+  await file.save()
+  console.log(file.downloadCount)
+
+  res.download(file.path, file.originalName)
+}
+
+app.listen(process.env.PORT)
